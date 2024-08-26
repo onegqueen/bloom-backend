46b29388a79844aca9ae20ad7c875128023256e2
coke98 <jhs9832@gmail.com>
2024-08-27 05:00:43 +0900

    chore: 스프링 부트 초기 설정
    ref: #1

---
+++ src/main/java/com/example/bloombackend/BloomBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.bloombackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloomBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BloomBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/bloombackend/BloomBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bloombackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloomBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

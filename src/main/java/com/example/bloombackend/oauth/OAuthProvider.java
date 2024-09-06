4375568f045d64413627e77c5bd4412ea7273137
kangjiwon <kangjiwon45@gmail.com>
2024-09-06 15:04:48 +0900

    style: 코드 포맷팅 수정 (줄바꿈 및 공백 정리)

--- src/main/java/com/example/bloombackend/oauth/OAuthProvider.java
+++ src/main/java/com/example/bloombackend/oauth/OAuthProvider.java
@@ -1,5 +1,5 @@
 package com.example.bloombackend.oauth;
 
 public enum OAuthProvider {
-    KAKAO
+	KAKAO
 }
--- src/main/java/com/example/bloombackend/user/entity/Gender.java
+++ src/main/java/com/example/bloombackend/user/entity/Gender.java
@@ -1,15 +1,15 @@
 package com.example.bloombackend.user.entity;
 
 public enum Gender {
-    M("남성"),F("여성"),O("기타");
+	M("남성"), F("여성"), O("기타");
 
-    private final String label;
+	private final String label;
 
-    Gender(String label) {
-        this.label = label;
-    }
+	Gender(String label) {
+		this.label = label;
+	}
 
-    public String getLabel() {
-        return label;
-    }
+	public String getLabel() {
+		return label;
+	}
 }

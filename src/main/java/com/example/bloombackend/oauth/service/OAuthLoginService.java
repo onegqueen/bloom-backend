5e6b678edcac80d8badc7a38a7be2396725796d6
coke98 <jhs9832@gmail.com>
2024-09-07 21:09:51 +0900

    feat: Jwt accessToken을 반환하도록 OAuthLoginService의 로그인 메서드 수정
    ref: #6

--- src/main/java/com/example/bloombackend/oauth/service/OAuthLoginService.java
+++ src/main/java/com/example/bloombackend/oauth/service/OAuthLoginService.java
@@ -1,5 +1,6 @@
 package com.example.bloombackend.oauth.service;
 
+import com.example.bloombackend.global.config.JwtTokenProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -10,17 +11,19 @@ import com.example.bloombackend.user.service.UserService;
 public class OAuthLoginService {
 	private final RequestKakaoInfoService requestKakaoInfoService;
 	private final UserService userService;
+	private final JwtTokenProvider jwtTokenProvider;
 
 	@Autowired
-	private OAuthLoginService(RequestKakaoInfoService requestKakaoInfoService, UserService userService) {
+	private OAuthLoginService(RequestKakaoInfoService requestKakaoInfoService, UserService userService, JwtTokenProvider jwtTokenProvider) {
 		this.requestKakaoInfoService = requestKakaoInfoService;
 		this.userService = userService;
+		this.jwtTokenProvider = jwtTokenProvider;
 	}
 
 	public String login(String authorizationCode) {
 		KakaoInfoResponse response = requestKakaoInfoService.request(authorizationCode);
 		Long userId = userService.findOrCreateUser(response);
-		return "genertaed-token";
+		return jwtTokenProvider.createAccessToken(userId.toString());
 	}
 
 	public String getKakaoLoginUrl() {

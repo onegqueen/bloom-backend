600b41433a35333d28c58b14f102abd0e8834f36
onegqueen <rkdwldnjs30@naver.com>
2024-09-08 22:35:47 +0900

    feat: 로그인 페이지 반환값 수정 - RedirectView -> String ref: #3

--- src/main/java/com/example/bloombackend/oauth/controller/OAuthController.java
+++ src/main/java/com/example/bloombackend/oauth/controller/OAuthController.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.view.RedirectView;
 
 import com.example.bloombackend.oauth.service.OAuthLoginService;
 
@@ -26,7 +25,7 @@ public class OAuthController {
 	}
 
 	@GetMapping("/kakao/login")
-	public RedirectView redirectToKakaoLogin() {
-		return new RedirectView(oAuthLoginService.getKakaoLoginUrl());
+	public String redirectToKakaoLogin() {
+		return oAuthLoginService.getKakaoLoginUrl();
 	}
 }

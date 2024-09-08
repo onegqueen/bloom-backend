f152cdaaa4afbaf313b0c47e609efa9b1f01d7fd
onegqueen <rkdwldnjs30@naver.com>
2024-09-08 21:36:28 +0900

    fix: api 요청 버그 수정 POST->GET ref: #3

--- src/main/java/com/example/bloombackend/oauth/controller/OAuthController.java
+++ src/main/java/com/example/bloombackend/oauth/controller/OAuthController.java
@@ -3,7 +3,6 @@ package com.example.bloombackend.oauth.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,7 +20,7 @@ public class OAuthController {
 		this.oAuthLoginService = oAuthLoginService;
 	}
 
-	@PostMapping("/oauth/kakao")
+	@GetMapping("/oauth/kakao")
 	public ResponseEntity<String> loginKakao(@RequestParam String code) {
 		return ResponseEntity.ok(oAuthLoginService.login(code));
 	}

36a283a712e8f3aee01b5f9929a9e949711bd63b
onegqueen <rkdwldnjs30@naver.com>
2024-09-12 17:51:56 +0900

    fix: Get -> Post 메소드 변경 ref: #18

--- src/main/java/com/example/bloombackend/oauth/controller/OAuthController.java
+++ src/main/java/com/example/bloombackend/oauth/controller/OAuthController.java
@@ -3,6 +3,7 @@ package com.example.bloombackend.oauth.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,7 +22,7 @@ public class OAuthController {
 		this.oAuthLoginService = oAuthLoginService;
 	}
 
-	@GetMapping("/kakao")
+	@PostMapping("/kakao")
 	public ResponseEntity<LoginResponse> loginKakao(@RequestBody KakaoLoginRequest request) {
 		return ResponseEntity.ok(oAuthLoginService.login(request.authorizationCode()));
 	}

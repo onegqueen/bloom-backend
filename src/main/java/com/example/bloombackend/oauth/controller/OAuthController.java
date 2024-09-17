dcc97ba865686c44c7dfea104fee7f46668d86fb
onegqueen <rkdwldnjs30@naver.com>
2024-09-17 17:58:46 +0900

    fix: resolve conflict

--- src/main/java/com/example/bloombackend/oauth/controller/OAuthController.java
+++ src/main/java/com/example/bloombackend/oauth/controller/OAuthController.java
@@ -1,10 +1,14 @@
 package com.example.bloombackend.oauth.controller;
 
-import com.example.bloombackend.oauth.controller.dto.request.KakaoLoginRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.example.bloombackend.oauth.controller.dto.request.KakaoLoginRequest;
 import com.example.bloombackend.oauth.controller.dto.response.LoginResponse;
 import com.example.bloombackend.oauth.service.OAuthLoginService;
 

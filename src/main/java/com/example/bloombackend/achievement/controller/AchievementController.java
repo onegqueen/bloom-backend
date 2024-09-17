3bd287aeb60b75e33bd0a3e0d454d9dd3c516a54
coke98 <jhs9832@gmail.com>
2024-09-18 07:59:01 +0900

    feat: 성취도 도메인 컨트롤러 추가
    ref: #8

---
+++ src/main/java/com/example/bloombackend/achievement/controller/AchievementController.java
@@ -0,0 +1,45 @@
+package com.example.bloombackend.achievement.controller;
+
+import com.example.bloombackend.achievement.controller.dto.request.AchievementLevelUpdateRequest;
+import com.example.bloombackend.achievement.controller.dto.request.FlowerRegisterRequest;
+import com.example.bloombackend.achievement.controller.dto.response.AchievementLevelUpdateResponse;
+import com.example.bloombackend.achievement.controller.dto.response.MonthlyDataResponse;
+import com.example.bloombackend.achievement.controller.dto.response.RecentSixMonthDataResponse;
+import com.example.bloombackend.achievement.service.AchievementService;
+import com.example.bloombackend.global.config.annotation.CurrentUser;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/achievement")
+public class AchievementController {
+    private final AchievementService achievementService;
+
+    public AchievementController(AchievementService achievementService) {
+        this.achievementService = achievementService;
+    }
+
+    @PostMapping("/flower")
+    public ResponseEntity<Void> setDailyFlower(@RequestBody FlowerRegisterRequest request,
+                                                 @CurrentUser Long userId) {
+        achievementService.setDailyFlower(userId, request);
+        return ResponseEntity.ok().build();
+    }
+
+    @PatchMapping("")
+    public ResponseEntity<AchievementLevelUpdateResponse> updateAchievementLevel(@RequestBody AchievementLevelUpdateRequest request,
+                                                                                 @CurrentUser Long userId) {
+        return ResponseEntity.ok(achievementService.updateAchievementLevel(userId, request));
+    }
+
+    @GetMapping("/monthly")
+    public ResponseEntity<MonthlyDataResponse> getMonthlyAchievements(@RequestParam String month,
+                                                                      @CurrentUser Long userId) {
+        return ResponseEntity.ok(achievementService.getMonthlyAchievements(userId, month));
+    }
+
+    @GetMapping("/recent-six-months")
+    public ResponseEntity<RecentSixMonthDataResponse> getRecentSixMonthsAchievements(@CurrentUser Long userId) {
+        return ResponseEntity.ok(achievementService.getRecentSixMonthsAchievements(userId));
+    }
+}
